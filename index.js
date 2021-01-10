b2d727775c79a42c59da51c8d4ed5b17b5a3e9ab
simonisfiction <59346539+simonisfiction@users.noreply.github.com>
2021-01-10 01:38:47 -0800

    add event listeners to hte buttons, and create a game loop

---
+++ index.js
@@ -0,0 +1,19 @@
+document.addEventListener("keydown", function() {
+  gameLoop();
+});
+
+var buttons = [];
+buttons = document.querySelectorAll("button");
+
+for(var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    
+  }
+}
+
+var fail = false;
+function gameLoop(){
+  while(!fail){
+
+  }
+}
